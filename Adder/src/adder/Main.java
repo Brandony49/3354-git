0e849c461e299e07fc7fbd811a724f144469aac6
Unknown <brandony49@gmail.com>
2017-01-31 23:27:09 -0600

    added exceptions to adder main
    added more exceptions to the adder program main

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -8,13 +8,28 @@ public class add {
             System.out.println(result);
         } catch (Exception e) {
             System.err.println("Please provide any number of integers to add");
+            System.err.println("Invalid character");
+          
         }
     }
 
     private static int addArguments(String[] args) {
       int sum = 0;
-      for (int i = 0; i <args.length; i++)
+      
+      if((args[0]) == "-"){
+       
+       for (int i = 1; i <args.length; i++)
+           sum = sum - Integer.valueOf(args[i]); 
+    } 
+  	  else{
+      	for (int i = 0; i <args.length; i++)
            sum = sum + Integer.valueOf(args[i]); 
+    } 
+     
       return sum;
     }
 }
+
+
+
+
